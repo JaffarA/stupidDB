91cea070bfaa5a76cc67d347f31ab5a37583a338
Jaffar Almaleki <jaffar.almaleki@gmail.com>
2024-01-26 03:01:32 +0000

    feat: added stupiddb

---
+++ stupiddb/__init__.py
@@ -0,0 +1,1 @@
+from .stupiddb import StupidDB
---
+++ stupiddb/stupiddb.py
@@ -0,0 +1,63 @@
+import json
+from os import remove
+from os.path import exists
+from typing import Any
+
+
+class StupidDB:
+    def __init__(self, path: str = "db.json") -> None:
+        self.path = path
+        self.local_db: list[dict] = []
+        if not exists(path):
+            self.write_to_db()
+        self.refresh_db()
+
+    def refresh_db(self) -> None:
+        try:
+            with open(self.path, "r") as f:
+                self.local_db = json.load(f)
+        except (FileNotFoundError, PermissionError) as e:
+            raise FileNotFoundError(f"Error reading from {self.path}: {e}")
+        except (TypeError, ValueError) as e:
+            raise ValueError(f"Error deserializing JSON: {e}")
+        except (UnicodeDecodeError, OSError, IOError) as e:
+            raise IOError(f"File I/O error: {e}")
+        except Exception as e:
+            raise Exception(f"An unexpected error occurred: {e}")
+
+    def write_to_db(self) -> None:
+        try:
+            with open(self.path, "w") as f:
+                json.dump(self.local_db, f)
+        except (FileNotFoundError, PermissionError) as e:
+            raise FileNotFoundError(f"Error reading from {self.path}: {e}")
+        except (TypeError, ValueError) as e:
+            raise ValueError(f"Error deserializing JSON: {e}")
+        except (UnicodeEncodeError, OSError, IOError) as e:
+            raise IOError(f"File I/O error: {e}")
+        except Exception as e:
+            raise Exception(f"An unexpected error occurred: {e}")
+
+    def delete_db(self) -> None:
+        self.local_db = []
+        remove(self.path)
+
+    def insert(self, data: Any) -> None:
+        self.refresh_db()
+        self.local_db.append(data)
+        self.write_to_db()
+
+    def update(self, index: int, data: Any) -> None:
+        self.refresh_db()
+        try:
+            self.local_db[index] = data
+        except IndexError:
+            raise IndexError("Index out of range")
+        self.write_to_db()
+
+    def retrieve(self, index: int) -> Any:
+        self.refresh_db()
+        try:
+            return self.local_db[index]
+        except IndexError:
+            raise IndexError("Index out of range")
